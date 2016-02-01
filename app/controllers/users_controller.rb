e5e9ac6161940c7ee65f4305d42882f84cbe842d
Christopher Dobler <chrisdobler@gmail.com>
2016-02-01 05:09:39 -0800

    Adds Previous 20 tweet + Username to User Page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
+    @tweets = $twitter_connect.user_timeline("casetabs")
   end
 
   # GET /users/new
